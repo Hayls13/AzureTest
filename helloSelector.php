fdd8323aa9f1c607550b5b3f849ef427f5f065ad
hayley <hayley@drumlithie.com>
2015-10-26 16:08:31 +0000

    added index.html

---
+++ helloSelector.php
@@ -0,0 +1,13 @@
+<?php
+
+    $query = $_GET["query"];
+    $type = $GET_["type"];
+    echo "The query is " . $cat . " and the type is " . $type";
+    ?>
+    
+/**
+ * Created by PhpStorm.
+ * User: 1300777
+ * Date: 26/10/2015
+ * Time: 16:01
+ */
