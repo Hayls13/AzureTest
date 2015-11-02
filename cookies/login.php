b1e0a24dfcb2ffcc585c3d0018e2fdedb199c8fb
hayley <hayley@drumlithie.com>
2015-11-02 14:56:53 +0000

    added index.html

--- cookies/login.php
+++ cookies/login.php
@@ -6,6 +6,7 @@ $password = "pass";
   if ($_POST["username"] = $username AND $_POST["password"] = $password){
       echo "logged in";
       setcookie('access_level', 'standarduser');
+      setcookie('$username', "hayley");
   } else{
       echo"nope";
   }
