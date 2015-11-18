9d59122269ec888c2465f8ae4814243fa3eb3ca3
hayley <hayley@drumlithie.com>
2015-11-18 12:09:49 +0000

    added index.html

--- lab2/superhero.php
+++ lab2/superhero.php
@@ -6,7 +6,7 @@ $lname = $_POST["surname"];
 $dob = $_POST["dob"];
 $sex = $_POST["gender"];
 
-echo "Name: " . $fname . $lname  . PHP_EOL . "Date of birth: " . $dob;
+echo "Name: " . $fname . $lname  . PHP_EOL . "Date of birth: " . $dob . "Sex: " . $sex;
 echo $sex;
 ?>
 </html>
