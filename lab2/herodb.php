a36d412b5b0d56b5aea9dbd95278849678092829
hayley <hayley@drumlithie.com>
2015-11-18 12:49:06 +0000

    added index.html

---
+++ lab2/herodb.php
@@ -0,0 +1,14 @@
+<?php
+
+$db = new mysqli(
+    "br-cdbr-azure-south-a.cloudapp.net",
+    "beff680d970100",
+    "29313567",
+    "hr1300777"
+);
+
+if($db->connect_errno){
+    die('Connectfailed'['.$db->connect-error.']);
+}
+
+?>
