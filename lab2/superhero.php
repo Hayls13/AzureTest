612a22f42fa92ead48db8de7faaa9d1d6f5de629
hayley <hayley@drumlithie.com>
2015-11-18 11:48:06 +0000

    added index.html

--- lab2/superhero.php
+++ lab2/superhero.php
@@ -6,7 +6,7 @@ $lname = $_POST["surname"];
 $dob = $_POST["birthdate"];
 $sex = $_POST["gender"];
 
-echo $fname . $lname;
-echo $dob;
+echo "Name: " . $fname . $lname  . "Date of birth: " . $dob;
 echo $sex;
 ?>
+</html>
