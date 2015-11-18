a386a085e05c9a67e5114c990e08a55f1d185a78
hayley <hayley@drumlithie.com>
2015-11-18 12:17:12 +0000

    added index.html

--- lab2/superhero.php
+++ lab2/superhero.php
@@ -5,8 +5,14 @@ $fname = $_POST["forename"];
 $lname = $_POST["surname"];
 $dob = $_POST["dob"];
 $sex = $_POST["sex"];
+$power1 = $_POST["power1"];
+$power2 = $_POST["power2"];
+$power3 = $_POST["power3"];
+$power4 = $_POST["power4"];
 
-echo "Name: " . $fname . $lname  . PHP_EOL . "Date of birth: " . $dob . "Sex: " . $sex;
-echo $sex;
+
+
+echo "Name: " . $fname . $lname  . PHP_EOL . "Date of birth: " . $dob . "Sex: " . $sex . "Powers: " . $power1 . $power2 . $power3 . $power4;
+
 ?>
 </html>
