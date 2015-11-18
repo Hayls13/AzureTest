886798931295a563c0dc01ea49c1005d2a4cba67
hayley <hayley@drumlithie.com>
2015-11-18 14:37:02 +0000

    added index.html

--- wikipage/wiki.php
+++ wikipage/wiki.php
@@ -1,10 +1,17 @@
 <?php
 
-$db =
+$db = new mysqli(
+    "br-cdbr-azure-south-a.cloudapp.net",
+    "beff680d970100",
+    "29313567",
+    "hr1300777"
+);
 
-/**
- * Created by PhpStorm.
- * User: 1300777
- * Date: 09/11/2015
- * Time: 14:58
- */
+if($db->connect_errno){
+    die('Connectfailed['.$db->connect-error.']');
+}
+echo "<p> Connection sucessful</p>";
+
+$sql_query = "SELECT * FROM articletext WHERE topicID='1'";
+
+?>
