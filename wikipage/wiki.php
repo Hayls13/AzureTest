43dd7c3a3193e81f18bf169d5b710b3a5a1f1b54
hayley <hayley@drumlithie.com>
2015-11-18 14:40:23 +0000

    added index.html

--- wikipage/wiki.php
+++ wikipage/wiki.php
@@ -13,5 +13,10 @@ if($db->connect_errno){
 echo "<p> Connection sucessful</p>";
 
 $sql_query = "SELECT * FROM articletext WHERE topicID='1'";
+$result = $db->query($sql_query);
+while($row = $result->fetch_array()){
+    echo "<p>" . $row['topidID'] . "</p>";
+}
+
 
 ?>
